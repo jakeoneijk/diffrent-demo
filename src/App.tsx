d8192bd44336057ae4e43321824826740ea0c739
AudAi <audai_jaekwon@AudAiui-MacBookPro.local>
2024-01-11 14:40:28 +0900

    add demo video

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,27 @@ import EnvToEnv from './Source/EnvToEnv';
 
 const style = {
   display: 'flex',
-  'flexDirection': 'column' as 'column'
+  'flexDirection': 'column' as 'column',
+  'alignItems': 'center' as 'center',
+}
+
+const iframeContainerStyle = {
+  'paddingTop': '50px',
+  
+
 }
 
 function App() {
   return (
     <div className="App" style={style}>
       <Title/>
+      <div style={iframeContainerStyle}>
+        <iframe width="640" height="400" src="https://www.youtube.com/embed/NYptYx7tMBY?si=VU2pXzrWSousMSVe" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+      </div>
       <Section name='Abstract' content={paperContent.abstract}/>
-      <div style={{margin: 'auto'}}><img style={{'width':'500px', padding:'40px'}} src={fig}/></div>
+      <div style={{margin: 'auto'}}>
+        <img style={{'width':'500px', padding:'40px'}} src={fig}/>
+      </div>
       <EnvToClean/>
       <CleanToEnv/>
       <EnvToEnv/>

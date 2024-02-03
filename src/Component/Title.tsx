543fac3853f0a1e01d17c63c82af97ae653f9b6a
AudAi <audai_jaekwon@AudAiui-MacBookPro.local>
2024-02-03 15:29:18 +0900

    update paper info

--- src/Component/Title.tsx
+++ src/Component/Title.tsx
@@ -1,9 +1,20 @@
 import { paperContent } from "../Config/PaperContent"
 
+const titleContainerStyle = {
+  'marginTop': '60px',
+  'marginLeft': '65px',
+  'marginRight': '65px',
+}
+
+const paperInfoStyle = {
+  'marginBottom': '15px', 
+  'fontFamily': 'Trebuchet MS, sans-serif',
+  'fontWeight': 400,
+  'fontSize': '15px',
+  color : '#5f6368'
+}
+
 const titleStyle = {
-  'paddingTop': '60px',
-  'paddingLeft': '65px',
-  'paddingRight': '65px',
   'textAlign': 'center' as 'center',
   'fontFamily': 'Trebuchet MS, sans-serif',
   'fontWeight': 600,
@@ -26,17 +37,46 @@ const affiliationStyle = {
     'fontSize': '21px'
 }
 
+const linkContainerStyle = {
+  'display': 'flex',
+  'justifyContent': 'center' as 'center',
+  'alignItems': 'center' as 'center',
+  'marginTop': '20px',
+
+}
+
+const buttonStyle = {
+  padding: '6px 14px',
+  backgroundColor: '#fff',
+  'border': '1px solid #f8f9fa',
+  'borderRadius': '24px',
+  'cursor': 'pointer',
+  'fontFamily': 'Arial, sans-serif',
+  'fontSize': '13px',
+  'fontWeight': 550,
+  boxShadow: 'rgba(0, 0, 0, .2) 0 3px 5px -1px,rgba(0, 0, 0, .10) 0 6px 10px 0,rgba(0, 0, 0, .08) 0 1px 18px 0',
+  color: '#3c4043'
+}
+
+function openLink(){
+  window.open(paperContent.paperLink, '_blank')
+}
+
 export default function Title(){
   return (
     <div>
-        <div style={titleStyle}>
-            {paperContent.title}
+        <div style={titleContainerStyle}>
+          <div style={paperInfoStyle}> {paperContent.paperInfo} </div>
+          <div style={titleStyle}> {paperContent.title} </div>
         </div>
         <div style={authorStyle}>
             {paperContent.author}
         </div>
         <div style={affiliationStyle}>
             {paperContent.affiliation}
+        </div>
+        <div style={linkContainerStyle}>
+          <div onClick={openLink} style={buttonStyle}> Paper </div>
         </div>
     </div>
   )
